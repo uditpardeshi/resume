064581faec2d23a5e7a4a12a9871eeb3471de3e1
unknown <uditpardeshi2007@gmail.com>
2026-06-07 09:07:10 +0530

    vercel deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from 'nitro/vite';
 import path from "path";
 
 export default defineConfig({
