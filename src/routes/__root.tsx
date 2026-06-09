3b05c384d60d24f38d62c87755397753b2ec9494
unknown <uditpardeshi2007@gmail.com>
2026-06-09 16:22:41 +0530

    Google1

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -71,6 +71,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "google-site-verification", content: "--uICDXum29cB3a_AE2l-keeORUN4CD6T65JjHNdbzo" },
       { title: "Resume / CV Builder — Free, Premium & ATS-Friendly" },
       {
         name: "description",
