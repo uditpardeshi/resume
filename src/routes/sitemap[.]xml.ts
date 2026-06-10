7778197cf44b97b5cc1c71038c41be02c6ca1cb1
unknown <uditpardeshi2007@gmail.com>
2026-06-10 09:27:32 +0530

    sitemap fixed 2

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -27,7 +27,7 @@ export const Route = createFileRoute("/sitemap.xml")({
         const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls.join("\n")}\n</urlset>`;
         return new Response(xml, {
           headers: {
-            "Content-Type": "text/xml; charset=utf-8",
+            "Content-Type": "application/xml; charset=utf-8",
             "Cache-Control": "public, max-age=3600",
           },
         });
